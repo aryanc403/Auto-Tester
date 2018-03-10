5dcf4f55803620d5cd44b748e38318a7b3163c28
Aryan Choudhary <32961797+aryanc403@users.noreply.github.com>
2018-03-10 23:48:27 +0530

    Added Crawler part1
    Added Crawler part1

--- AdvanceTrackProject/AdvanceTrackProject/MainWindow.xaml.cs
+++ AdvanceTrackProject/AdvanceTrackProject/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Net.Http;
+using HtmlAgilityPack;
 
 namespace AdvanceTrackProject
 {
@@ -38,11 +41,41 @@ namespace AdvanceTrackProject
             Nullable<bool> isOpened = dlg.ShowDialog();
 
             //if file selected
-            if(isOpened == true)
+            if (isOpened == true)
             {
                 string fileName = dlg.FileName;
                 fileTb.Text = fileName;
             }
+        }
+
+        private void fetchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StartCrawlerAsync();
+        }
+
+        private static async Task StartCrawlerAsync()
+        {
+            var url = "http://codeforces.com/problemset/problem/950/B";//sample url must change before final submit
+            //var url = "http://codeforces.com/problemset/problem/949/B";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var inputs = htmlDocument.DocumentNode.Descendants("div")
+                .Where(node => node.GetAttributeValue
+                ("class", "")
+                    .Equals("input")).ToList();
+
+            var outputs = htmlDocument.DocumentNode.Descendants("div")
+                            .Where(node => node.GetAttributeValue
+                            ("class", "")
+                                .Equals("output")).ToList();
+
+
+
+            //Console.WriteLine("Aryan");
         }
     }
 }
